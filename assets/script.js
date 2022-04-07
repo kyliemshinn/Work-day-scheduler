380343f0ad68933c93430b5c52e24faa0bc16f89
kylie <kyliemshinn@gmail.com>
2022-04-07 09:16:02 -0700

    clean up Javascript code, fix local storage to stay on page after refresh

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,45 @@
 //global variables that will be needed
-var saveButton = $(".saveBtn");
-var timeBlock = $(".time-block");
+// var saveButton = $(".saveBtn");
+// var timeBlock = $(".time-block");
 
-var scheduleInfo;
-var scheduleTime;
+// var scheduleInfo;
+// var scheduleTime;
 
 //variables for each hour
-var hourNineAm = $("#nine-am");
-var hourTenAm =  $("#ten-am");
-var hourElevenAm = $("#eleven-am");
-var hourTwelvePm = $("#twelve-pm");
-var hourOnePm = $("#one-pm");
-var hourTwoPm = $("#two-pm");
-var hourThreePm = $("#three-pm");
-var hourFourPm = $("#four-pm");
-var hourFivePm  = $("#five-pm");
+// var hourNineAm = $("#nine-am");
+// var hourTenAm =  $("#ten-am");
+// var hourElevenAm = $("#eleven-am");
+// var hourTwelvePm = $("#twelve-pm");
+// var hourOnePm = $("#one-pm");
+// var hourTwoPm = $("#two-pm");
+// var hourThreePm = $("#three-pm");
+// var hourFourPm = $("#four-pm");
+// var hourFivePm  = $("#five-pm");
 
 
 
 
+//WHEN I click the save button for that timeblock
+$(".saveBtn").on("click", function(){
+
+   var scheduleTime = $(this).siblings(".description").val();
+    var scheduleInfo = $(this).parent().attr("id");
+    localStorage.setItem(scheduleInfo, scheduleTime);
+    // saveData();
+});
 
 //the current day and time is displayed at the top of the page, rerouting every second to have active running clock
-function displayTime() {
+// function displayTime() {
 
-    setInterval(()=> {
-        var time = moment().format("MMMM Do, YYYY h:mm:ss a");
-        $("#currentDay").text(time);
-        }, 1000)
+//     setInterval(()=> {
+//         var time = moment().format("MMMM Do, YYYY h:mm:ss a");
+//         $("#currentDay").text(time);
+//         }, 1000)
 
-}
+// }
+
+
+$("#currentDay").text(moment().format('dddd, MMMM Do'));
 //depending on the time of day, the attributes need to be set-take the numbers on the side 
 
 //WHEN I view the timeblocks for that day
@@ -37,82 +48,43 @@ function timeTrackerWithColor() {
     //setting up for the comparison of the current hours
     var currentHour = moment().hour();
 
-    timeBlock.each(function() {
-
-        var timeBlockHours = parseInt($(this));
+    $(".time-block").each(function() {
+//remove dash on the ID of time-block so does not identify as two IDs. Targeting the number that was turned from a string
+        var timeBlockHours = parseInt($(this).attr("id").split('-')[1]);
 //comparison of the block time with the current time to set the CSS of the time blocks
-        if (timeBlockHours > currentHour) {
-            $(this).addClass("future");
+        if (timeBlockHours < currentHour) {
+            $(this).addClass("past");
         } else if (timeBlockHours === currentHour) {
+            $(this).removeClass("past");
             $(this).addClass("present");
         } else {
-            $(this).addClass("past");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
         }
     
     });
+}
 
-
-//WHEN I click the save button for that timeblock
-saveButton.click(function(){
-
-    scheduleInfo = $(this).siblings(".description").val();
-    scheduleTime = $(this).parent();
-    localStorage.setItem(scheduleInfo, scheduleTime);
-    saveData();
-});
+timeTrackerWithColor();
+var interval = setInterval(timeTrackerWithColor, 15000)
     
 //THEN the text for that event is saved in local storage
-function saveData() {
+//value from local storage will go in to parent line but will be saved in text area
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    // var nineAm = JSON.stringify(localStorage.getItem("nine-am"));
-    // hour9.val(hourNineAm);
 
-    // var tenAm = JSON.parse(localStorage.getItem("ten-am"));
-    // hour10.val(hourTenAm);
 
-    // var elevenAm = JSON.parse(localStorage.getItem("eleven-am"));
-    // hour11.val(hourElevenAm);
-
-    // var twelvePm = JSON.parse(localStorage.getItem("twelve-pm"));
-    // hour12.val(hourTwelvePm);
-
-    // var onePm = JSON.parse(localStorage.getItem("one-pm"));
-    // hour13.val(hourOnePm);
-
-    // var twoPm = JSON.parse(localStorage.getItem("two-pm"));
-    // hour14.val(hourTwoPm);
-
-    // var threePm = JSON.parse(localStorage.getItem("three-pm"));
-    // hour15.val(hourThreePm);
-
-    // var fourPm = JSON.parse(localStorage.getItem("four-pm"));
-    // hour16.val(hourFourPm);
-
-    // var fivePm = JSON.parse(localStorage.getItem("five-pm"));
-    // hour17.val(hourFivePm);
-    
-
-    // window.saveData = function() {
-    //     localStorage.setItem.json.stringify(scheduleInfo, scheduleTime.val());
-        
-    localStorage.setItem(scheduleTime, scheduleInfo);
-    // localStorage.getItem(scheduleInfo, scheduleTime);
-
-    $(".description").val(localStorage.getItem(hourNineAm));
-    $(".description").val(localStorage.getItem(hourTenAm));
-    $(".description").val(localStorage.getItem(hourElevenAm));
-    $(".description").val(localStorage.getItem(hourTwelvePm));
-    $(".description").val(localStorage.getItem(hourOnePm));
-    $(".description").val(localStorage.getItem(hourTwoPm));
-    $(".description").val(localStorage.getItem(hourThreePm));
-    $(".description").val(localStorage.getItem(hourFourPm));
-    $(".description").val(localStorage.getItem(hourFivePm));
-// }
-}
-}
     
     
 //WHEN I refresh the page
 //THEN the saved events persist
-timeTrackerWithColor();
-displayTime();
+// displayTime();
