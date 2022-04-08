bde223e8ca15b16aeceb64765cc0a07e883a9ac4
kylie <kyliemshinn@gmail.com>
2022-04-07 17:13:38 -0700

    upload gif to README.md, clean up code, comment Javascript

--- README.md
+++ README.md
@@ -4,20 +4,26 @@
 
 This webpage is meant to display a current daily schedule that can be cleared out after each day with a clear button. The timing is set for a typical workday- 9am to 5pm- and when you input text for a given hour it will stay there until the information is cleared out. The schedule is color coordinated, with red being the current hour, grey being the past, and green being the future. There is a clock displayed at the top of the page where it is counting each second.
 
------insert gif once finished-----
+![work-day-schedule](/work-day-schedule-gif.gif)
 
 ### Lessons Learned
 
-The most prominent lesson learned with this project is to continuously console.log parts of code to ensure that it is working properly.
+The most prominent lesson learned with this project is to continuously console.log parts of code to ensure that it is working properly.Looking at things piece by piece always finds itself incredibly useful and creating a clear plan in th beginning to get an idea of all the parts that willbe needed.
 
 ### Author
 
 Kylie Shinn
 
-LinkedIn:
-Github:
+LinkedIn: ![LinkedInURL](https://www.linkedin.com/in/kylie-shinn-18b0301b7/)
+Github:![Github](https://github.com/kyliemshinn)
 
 
 ### License
 
+Copyright (c) [2022] [Kylie]
 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,24 @@
-//global variables that will be needed
-// var saveButton = $(".saveBtn");
-// var timeBlock = $(".time-block");
+//the current day and time is displayed at the top of the page, rerouting every second to have active running clock
+function displayTime() {
 
-// var scheduleInfo;
-// var scheduleTime;
+    setInterval(()=> {
+        var time = moment().format("MMMM Do, YYYY h:mm:ss a");
+        $("#currentDay").text(time);
+        }, 1000)
 
-//variables for each hour
-// var hourNineAm = $("#nine-am");
-// var hourTenAm =  $("#ten-am");
-// var hourElevenAm = $("#eleven-am");
-// var hourTwelvePm = $("#twelve-pm");
-// var hourOnePm = $("#one-pm");
-// var hourTwoPm = $("#two-pm");
-// var hourThreePm = $("#three-pm");
-// var hourFourPm = $("#four-pm");
-// var hourFivePm  = $("#five-pm");
+}
 
 
-
-
-//WHEN I click the save button for that timeblock
+//WHEN I click the save button for that timeblock it will save to the page
 $(".saveBtn").on("click", function(){
 
    var scheduleTime = $(this).siblings(".description").val();
     var scheduleInfo = $(this).parent().attr("id");
     localStorage.setItem(scheduleInfo, scheduleTime);
-    // saveData();
+   
 });
 
-//the current day and time is displayed at the top of the page, rerouting every second to have active running clock
-// function displayTime() {
 
-//     setInterval(()=> {
-//         var time = moment().format("MMMM Do, YYYY h:mm:ss a");
-//         $("#currentDay").text(time);
-//         }, 1000)
-
-// }
-
-
-$("#currentDay").text(moment().format('dddd, MMMM Do'));
-//depending on the time of day, the attributes need to be set-take the numbers on the side 
 
 //WHEN I view the timeblocks for that day
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
@@ -65,9 +43,9 @@ function timeTrackerWithColor() {
     
     });
 }
-
+//rechecks the coloring for the schedule every ten seconds to auto generate new colors
 timeTrackerWithColor();
-var interval = setInterval(timeTrackerWithColor, 15000)
+var interval = setInterval(timeTrackerWithColor, 10000)
     
 //THEN the text for that event is saved in local storage
 //value from local storage will go in to parent line but will be saved in text area
@@ -81,10 +59,21 @@ var interval = setInterval(timeTrackerWithColor, 15000)
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
+//clear button to clear out the schedule when neeeded at the bottom
+$("#clear").on("click", function() {
+ 
+    $("#hour-9 .description").val(localStorage.clear("hour-9"));
+    $("#hour-10 .description").val(localStorage.clear("hour-10"));
+    $("#hour-11 .description").val(localStorage.clear("hour-11"));
+    $("#hour-12 .description").val(localStorage.clear("hour-12"));
+    $("#hour-13 .description").val(localStorage.clear("hour-13"));
+    $("#hour-14 .description").val(localStorage.clear("hour-14"));
+    $("#hour-15 .description").val(localStorage.clear("hour-15"));
+    $("#hour-16 .description").val(localStorage.clear("hour-16"));
+    $("#hour-17 .description").val(localStorage.clear("hour-17"));
 
-
+});
     
     
-//WHEN I refresh the page
-//THEN the saved events persist
-// displayTime();
+
+displayTime();
